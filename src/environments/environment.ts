f64b83eb719591c4cea0c6ae87c8411c83c74263
Miguel E. Cabrera <mecg1994@gmail.com>
2025-11-21 07:28:11 -0600

    cambio server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-    apiUrl: 'https://localhost:7176' //
+    //apiUrl: 'https://localhost:7176' //
 
- //apiUrl: 'http://srvgtwdhm16:7176'
+ apiUrl: 'http://srvgtwdhm16:7176'
 };
